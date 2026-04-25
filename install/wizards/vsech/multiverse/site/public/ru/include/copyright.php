f5ed34e209eb3e72013dd468e5d9b820f6cedb37
Vsevolod Chechun <vsevche@gmail.com>
2026-04-25 13:20:19 +0500

    encode fix

--- install/wizards/vsech/multiverse/site/public/ru/include/copyright.php
+++ install/wizards/vsech/multiverse/site/public/ru/include/copyright.php
@@ -1,2 +1,2 @@
-В© 2017 РЁР°Р±Р»РѕРЅ СЃР°Р№С‚Р° РїРѕСЂС‚С„РѕР»РёРѕ.
-<span>Р Р°Р·СЂР°Р±РѕС‚РєР° <a href="https://vsech.tk/" target="_blank">Vsevolod Chechun</a></span>
+© 2017 Шаблон сайта портфолио.
+<span>Разработка <a href="https://bxcv.ru/" target="_blank">Vsevolod Chechun</a></span>
